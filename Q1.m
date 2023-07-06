982ae1c637434f4e276ecd8988a593e170ebe2d7
LFRezende <lfsrsprofessional@gmail.com>
2023-07-06 17:52:46 -0300

    Add: New Files and Changed the Source Code for Modulation Comparison

--- Q1.m
+++ Q1.m
@@ -1,139 +1,262 @@
 %% Exame Final de EET-50: Luis Felipe Silva Rezende Soares %%
 % Instituto Tecnológico de Aeronáutica, Junho de 2023 %
-% Introdução a Sistemas de Comunicação %
+% Introdução a Sistemas de Comunicação
 
-%% Questão 1 %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Utilized for the Final Exam of Communication Systems (EET-50)) in the Aeronautics Institute of Technology,ITA, Brazil %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Estime, atraves de simulacao computacional (m ́etodo de Monte Carlo),
-% a probabilidade de erro de sımbolo (Pe) em fun ̧c ̃ao da raz ̃ao Eb/N0, para
-% um canal AWGN de media zero e PSD N0/2, para as modulacoes em banda
-% base a seguir:
-% a) 2-PAM (BPSK)
-% b) 4-PAM
-% c) 4-QAM (QPSK)
-% d) 2-FSK
-% Obs: Os resultados das simula ̧c ̃oes devem ser comparados com os
-% resultados te ́oricos, na mesma figura de Pe × (Eb/N0)
+%% ### ITEM A ### --- Obtencao do Probabilidade de Erro %%
+format long;
 
-%% Resolução da Questão 1 %%
-clear all; clc;
+% Mensagem %
+clear all;
+clc;
+close all;
 
-% Calculo da energia de simbolo%
-delta = 1;
-M = 2;                          % Modulacao 2-PAM
-N = 4;                          % Modulacao 4-PAM
-S = 4;                          % Modulacao 4-QAM
-F = 2;                          % Modulacao 2-FSK
-Es = (delta^2)*(M^2 - 1)/12;    % Energia do Simbolo de 2-PAM
-Es_4PAM = (delta^2)*(N^2-1)/12; % Energia do Simbolo de 4-PAM
-Es_4QAM = (delta^2)*(S-1)/6;    % Energia do Símbolo de 4-QAM
-Es_2FSK = (delta^2);            % Energia do Símbolo de 2-FSK (='s)
+% Parametros de Iteracao % 
+EbN0min = -2;
+EbN0max = 16;
+quantidade = 100;
+errorCounter = 0;
+probErro2Pam = [];
+EbN0vector = EbN0min:(EbN0max - EbN0min)/quantidade:EbN0max;
 
-% Calculo da energia de bit %
+% Modulacao 2PAM:
+m = 2;
+Es = 1;                 % Energia de Simbolo Es = (1^2 + 1^2)/2 = 1;
+N = 1000;               % Iteracao em Monte Carlo
+len_mensagem = 1280;    % Comprimento da Mensagem enviada (Simbolos)
+mensagem = randi([0, m-1], [1, len_mensagem]);  % Mensagem
 
-K_2PAM = ceil(log(M)/log(2));           % Ceiling de numero fracionario
-Eb0 = Es/K_2PAM;                        % Energia do Bit de 2PAM
-K_4PAM = ceil(log(N)/log(2));           % Ceiling de numero fracionario de 4PAM
-Eb0_4PAM = Es_4PAM/K_4PAM;              % Energia do Bit de 4PAM
-K_4QAM = ceil(log2(S));                 % Ceiling do Bit de 4QAM
-Eb0_4QAM = Es_4QAM/K_4QAM;              % Energia do Bit de 4QAM
-K_2FSK = ceil(log2(F));                 % Ceiling do Bit de 2FSK
-Eb0_2FSK = Es_2FSK/K_2FSK;              % Energia do Bit de 2FSK
+% Vamos plotar o comportamento da modulacao conforme aumenta-se o Eb/N0.%
 
+for q=1:quantidade
+    % Contador de Erros Totais Obtidos apos Monte Carlo %
+    errorCounter = 0; 
+    %%% Laco da Rotina Iterativa de Monte Carlo %%%
+    for i= 1:N
+        
+        % Selecao de Eb/N0 para ruido construido %
+        EbN0 = EbN0vector(q);                               
+        mensagemPAM = pammod(mensagem, m);     % Modulacao PAM  
 
-% Parametros Iniciais de Calculo $
-mu = 0;
-sigma = 1;
-delta_n = 10^-2;
+        % Construcao do Ruido por Energia de Simbolo e Adicao %
+        ruido = (sqrt(Es/(2*(10^(EbN0/10))*log2(m))))*randn(size(mensagem));
+        mensagemPAMruidosa = mensagemPAM + ruido;
 
-% Inicializacao dos Vetores e Valores %
-Q_v = [];                       % Vetor para as integrais em Eb/N0;
-Q_v_4PAM = [];                  % Vetor para as integrais em Eb/N0 de 4-PAM;
-Q_v_4QAM = [];
-Q_v_2FSK = [];
-P_erro_2PAM_v = [];             % Vetor para as probabilidades de erro 2PAM
-P_erro_4PAM_v = [];             % Vetor para as probabilidades de erro 4PAM
-P_erro_4QAM_v = [];
-P_erro_2FSK_v = [];
-Ebv = [];                       % Vetor para os valores de Energia de Bit de 2PAM
-Ebv_4PAM = [];                  % Vetor para os valores de Energia de Bit de 4PAM
-Ebv_4QAM = [];
-Ebv_2FSK = [];
-P_e_2PAM_DB = [];               % Vetor para os valores de DB de 2PAM
-P_e_4PAM_DB = [];               % Vetor para os valores de DB de 4PAM
-P_e_4QAM_DB = [];
-P_e_2FSK_DB = [];
-N0 = 2*Eb0;                     % A razão Eb/N0 inicia em -3 dB, em que Eb é igual à PSD do ruido AWGN.
-IterLimit = 100*Eb0/delta_n + 1; % Limite de Interações: 30 dB %
-
-%% Loop de Construção dos Vetores %%
-
-% Iteração para ambas as modulacoes %
-for i = 1:IterLimit
-
-    Eb = Eb0 + i*delta_n;                       % Atualizacao da Energia do Bit de 2-PAM
-    Eb_4PAM = Eb0_4PAM + i*delta_n;             % Atualizacao da Energia do Bit de 4-PAM
-    Eb_4QAM = Eb0_4QAM + i*delta_n;             % Atualizacao da Energia do Bit de 4-QAM
-    Eb_2FSK = Eb0_2FSK + i*delta_n;             % Atualizacao da Energia do Bit de 2-FSK
-    Ebv(i) = 10*log10(Eb/N0);                   % 2-PAM em DB
-    Ebv_4PAM(i) = 10*log10(Eb_4PAM/N0);         % 4-PAM em DB    
-    Ebv_4QAM(i) = 10*log10(Eb_4QAM/N0);         % 4-QAM em DB
-    Ebv_2FSK(i) = 10*log10(Eb_2FSK/N0);         % 2-FSK em DB
-
-% O calculo da probabilidade de erro de uma modulação M-PAM é dada por:
-x = sqrt((6*(log2(M))/(M^2-1))*(Eb/N0));
-lowerLimit_2PAM = x;
-y = sqrt((6*(log2(N))/(N^2-1))*(Eb_4PAM/N0));
-lowerLimit_4PAM = y;
-upperLimit = Inf;
-
-% Definindo a Funcao Gaussiana de Densidade de Probabilidade %
-q = @(x) normpdf(x, mu, sigma);
-
-% Calculando a integral da Gaussiana para Modulacao 2-PAM %
-Q = integral(q, lowerLimit_2PAM, upperLimit);    % Integral
-Q_v(i) = Q;                                 % Push em Vetor de 2-PAM
-P_erro_2PAM = 2*(1 - (1/M))*Q;              % Probabilidade de 2-PAM
-P_erro_2PAM_v(i)= P_erro_2PAM;              % Push no Vetor de Probabilidade
-P_e_2PAM_DB(i) = 10*log10(P_erro_2PAM);     % Push no Vetor de Probabilidade em DB
-
-% Calculando a integral da Gaussiana para Modulacao 4-PAM %
-Q = integral(q, lowerLimit_4PAM, upperLimit);
-Q_v_4PAM(i) = Q;
-P_erro_4PAM = 2*(1 - (1/N))*Q;
-P_erro_4PAM_v(i) = P_erro_4PAM;
-P_e_4PAM_DB(i) = 10*log10(P_erro_4PAM);
-
-%  % Vamos agora calcular para uma Modulacao 4-QAM %  %
-
-% Calculando a integral da Gaussiana para Modulacao 4-QAM
-lowerLimit_4QAM = sqrt((3*log2(S)/(S-1))*(Eb_4QAM/N0));
-Q = integral(q, lowerLimit_4QAM, upperLimit);
-Q_v_4QAM = Q;
-P_erro_4QAM = 4*(1 - (1/sqrt(S)))*Q - 4*((1 - (1/sqrt(S)))^2)*Q^2;
-P_erro_4QAM_v(i) = P_erro_4QAM;
-P_e_4PAM_DB(i) = 10*log10(P_erro_4QAM);
-
-% % Vamos calcular para a Modulacao 2-FSK % %
-lowerLimit_2FSK = sqrt(Eb_2FSK/N0);
-Q = integral(q, lowerLimit_2FSK, upperLimit);
-Q_v_2FSK = Q;
-P_erro_2FSK = Q;
-P_erro_2FSK_v(i) = P_erro_2FSK;
-P_e_2FSK_DB(i) = 10*log10(P_erro_2FSK);
+        % Demodulacao PAM %
+        mensagemDemodPAM = pamdemod(mensagemPAMruidosa, m);             
+    
+        % Analise: Diferencas entre o sinal recebido e o sinal original.%
+        errorCounter = errorCounter + sum(mensagemDemodPAM ~= mensagem);
+    end
+    % Computo de Probabilidade%
+    probErro2Pam(q) = errorCounter/(len_mensagem*N);
 end
 
-%% Plot da Figura com Todas as Modulacoes %%
+% Plot da Teorica 2-PAM %%
+
+% Parametros Basicos
+m = 2; % m-PAM
+% Conjunto de Dados para o Plot da Teorica % 
+
+EbN0linear = 10.^(EbN0vector./10);
+
+% Probabilidade teorica 
+argumento = sqrt(6*(log2(m)/(m^2-1)).*(EbN0linear));
+Pe_t = 2*(1 - 1/m)*qfunc(argumento); % Formula Teorica de Probabilidade
+
+% Plot da Figura
 figure();
-semilogy(Ebv, P_erro_2PAM_v);
+semilogy(EbN0vector, Pe_t, 'b');
 hold on;
-xlabel('Eb/N0 (dB)');
-ylabel('SER');
-title('SER vs Eb/N0');
-semilogy(Ebv, P_erro_4PAM_v);
-semilogy(Ebv, P_erro_4QAM_v)
-semilogy(Ebv, P_erro_2FSK_v);
-ylim([10^-6, 0.5]);
-legend('2-PAM', '4-PAM','4-QAM','2-FSK', 'Location', 'southwest');
 grid on;
+xlabel('Eb/No (dB)');
+ylabel('Probabilidade de Erro');
+title('Probabilidade de Erro 2-PAM');
+semilogy(EbN0vector(1:length(probErro2Pam)), probErro2Pam, 'r');
+axis([-2, 10, 10^-5, 1]);
+legend('Teórico', 'Experimental');
+hold off;
+
+
+
+%% ### ITEM B ### --- Calculo da Modulacao 4-PAM %%
+
+% Parametros de Iteracao % 
+EbN0min = -2;
+EbN0max = 16;
+quantidade = 100;
+errorCounter = 0;
+probErro2Pam = [];
+EbN0vector = EbN0min:(EbN0max - EbN0min)/quantidade:EbN0max;
+probErro4Pam = [];
+% Modulacao 4PAM:
+m = 4;
+mensagem = randi([0, m-1], [1, len_mensagem]);
+% Energia de Símbolo: 3^2 + 1^2 + 1^2 + 3^2 = 20 --> Es = 20/4.
+Es = 5;  % Energia do Simbolo
+
+% Vamos plotar o comportamento da modulacao conforme aumenta-se o Eb/N0.
+for q = 1:quantidade
+    errorCounter = 0;
+    for i= 1:N
+                                                                       
+        EbN0 = EbN0vector(q);                                                              
+        
+        % Normalização da Mensagem e feita no ruido. %
+        mensagemPAM = pammod(mensagem, m);
+        ruido = (sqrt(Es/(2*(10^(EbN0/10))*log2(m))))*randn(size(mensagem));
+        mensagemPAMruidosa = mensagemPAM + ruido;
+        mensagemDemodPAM = pamdemod(mensagemPAMruidosa, m);
+    
+        % Analise: Diferencas entre o sinal recebido e o sinal original.%
+        errorCounter = errorCounter + sum(mensagemDemodPAM ~= mensagem);     
+    end
+    probErro4Pam(q) = errorCounter/(len_mensagem*N);
+end
+
+
+
+% Plot da Teorica 4-PAM %
+
+% Parametros Basicos
+m = 4; % m-PAM
+EbN0linear = 10.^(EbN0vector./10);
+
+% Calcular a probabilidade de erro teorica
+argumento = sqrt(6*(log2(m)/(m^2-1)).*(EbN0linear));
+
+% Normalizacao pela Energia de Simbolo.
+%Eb = Es/K => eS = KEb --> M-pam: 
+Pe_t = 2*(1 - 1/m)*qfunc(argumento); % Funcao Q
+
+% Curva teorica plotada
+figure();
+semilogy(EbN0vector, Pe_t, 'black');
+hold on;
+grid on;
+xlabel('Eb/No (dB)');
+ylabel('Probabilidade de Erro');
+title('Probabilidade de Erro da 4-PAM');
+semilogy(EbN0vector(1:length(probErro4Pam)), probErro4Pam, 'green');
+axis([-2, 14, 10^-5, 1]);
+legend('Teórico', 'Experimental');
+hold off;
+
+%% ### ITEM C ###  --- Calculo da Probabilidade de Erro de Modulacao QAM %%
+
+
+% Parametros de Iteracao % 
+EbN0min = -2;
+EbN0max = 16;
+errorCounter = 0;
+probErro4QAM = [];
+% Modulacao 4-QAM:
+m = 4;
+mensagem = randi([0, m-1], [1, len_mensagem]);
+% Cálculo da Energia de Símbolo: É o dobro da sqrt(4) - PAM, logo o dobro
+% da 2-PAM
+Es = 2;
+
+% Vamos plotar o comportamento da modulacao conforme aumenta-se o Eb/N0.
+for q=1:quantidade
+    errorCounter = 0;  
+    for i= 1:N
+        EbN0 = EbN0vector(q); 
+
+        % Modulacao, ruido e demodulacao %
+        mensagemQAM = qammod(mensagem, m); 
+        ruido = (sqrt(Es/(2*(10^(EbN0/10))*log2(m))))*randn(size(mensagem)) ... 
+            + 1j*(sqrt(Es/(2*(10^(EbN0/10))*log2(m))))*randn(size(mensagem));
+        mensagemQAMruidosa = mensagemQAM + ruido; 
+        mensagemDemodQAM = qamdemod(mensagemQAMruidosa, m);
+    
+        % Analise: Diferencas entre o sinal recebido e o sinal original.%
+        errorCounter = errorCounter + sum(mensagemDemodQAM ~= mensagem);
+
+    end
+    probErro4QAM(q) = errorCounter/(len_mensagem*N);
+end
+
+
+% Plot da Curva 4-QAM %
+
+                                                                   
+EbN0linear = 10.^(EbN0vector./10);
+
+% Calcular a probabilidade de erro teorica
+argumento = sqrt(3*(log2(m)/(m-1)).*(EbN0linear));
+Pe_t = 4*(1 - 1/sqrt(m)).*qfunc(argumento) - 4*(1 - 1/sqrt(m))^2.*qfunc(argumento).^2; % Funcao Q
+
+% Curva teorica plotada
+figure();
+semilogy(EbN0vector, Pe_t);
+hold on;
+grid on;
+xlabel('Eb/No (dB)');
+ylabel('Probabilidade de Erro');
+title('Probabilidade de Erro da 4-QAM');
+semilogy(EbN0vector(1:length(probErro4QAM)), probErro4QAM);
+axis([-2, 10, 10^-5, 1]);
+legend('Teórico', 'Experimental');
+hold off;
+
+
+%% ### ITEM D ###  - Modulacao FSK %%
+m = 2;         % Modulacao 2-FSK
+k = log2(m);   % Bits
+EbNo = 5;      % Eb/No (dB)
+Fs = 16;       % Taxa de Amostragem
+nsamp = 8;     % Numero de Amostras por Simbolo
+freqsep = 10;  % Separacao em Frequencia 
+
+len_mensagem = 128;
+N = 100;
+mensagem = randi([0, m-1], [1, len_mensagem]);
+
+
+% Parametros de Iteracao % 
+EbN0min = -2;
+EbN0max = 16;
+probErro2FSK = [];
+EbN0vector = EbN0min:(EbN0max - EbN0min)/quantidade:EbN0max;
+Es = 1; % A energia de simbolo do sinal BFSK é a propria amplitude do simbolo.
+
+% Modulacao 2FSK
+for q=1:quantidade
+    taxaDeErro = 0;
+    for i= 1:N
+    
+        EbN0 = EbN0vector(q);                                                                 
+        mFSK = fskmod(mensagem,m,freqsep,nsamp,Fs);
+        mFSKRuidosa  = awgn(mFSK,(EbN0)+10*log10(k)-10*log10(nsamp),'measured',[],'dB');
+        mFSKDemod = fskdemod(mFSKRuidosa,m,freqsep,nsamp,Fs);
+
+        %   Para a modulacao 2FSK, a existencia de 1 bit torna BER = SER %
+        [num,BER] = biterr(mensagem,mFSKDemod);
+
+        taxaDeErro = taxaDeErro + BER;
+        
+        
+    end
+    %   Probabilidade de Erro Experimental %
+    probErro2FSK(q) = taxaDeErro/N;
+
+    %   Vetor de Probabilidade de Erro Teorico (em 2-FSK, BER = SER)%
+    BER_teorico= berawgn(EbN0,'fsk',m,'noncoherent');
+    Pe_t(q) = BER_teorico;
+end
+
+% Plot da Curva para 2-FSK
+figure();
+semilogy(EbN0vector(1:length(Pe_t)), Pe_t);
+hold on;
+semilogy(EbN0vector(1:length(probErro2FSK)), probErro2FSK);
+grid on;
+xlabel('Eb/No (dB)');
+ylabel('Probabilidade de Erro');
+title('Probabilidade de Erro da 2-FSK');
+legend('Teórico', 'Experimental');
 hold off;
